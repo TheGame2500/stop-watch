7b7b71920eb0e9dffa0fb7f7925ec5f90c4032a4
Gabriel Bira <gabriel@littledata.io>
2020-04-05 18:16:19 +0300

    Add specs for watch mode

--- spec/core.spec.js
+++ spec/core.spec.js
@@ -42,14 +42,26 @@ function basicFunctionalityTests() {
 		consoleStub.log.should.have.been.calledWith(expectedLog)
 	})
 	it('logs properly since lastLap', () => {
-		const now = stopWatch.lap('suf')
-		const ms = now - stopWatch.start
+		const firstNow = stopWatch.lap('suf')
+		const secondNow = stopWatch.lap('suf')
+		const ms = secondNow - firstNow
 		const expectedLog = 'stop-watch testId pref ' + ms + ' ms since last lap suf'
 		consoleStub.log.should.have.been.calledWith(expectedLog)
 	})
 }
 
 function watcherFunctionalityTests() {
+	const consoleStub = {
+		log: sinon.stub(),
+		warn: sinon.stub(),
+		error: sinon.stub(),
+	}
+	beforeEach(() => {
+		consoleStub.log.reset()
+		consoleStub.warn.reset()
+		consoleStub.error.reset()
+	})
+
 	it('throws error if watch-mode and warn or error not present on logger', () => {
 		try {
 			new StopWatch({
@@ -59,5 +71,68 @@ function watcherFunctionalityTests() {
 		} catch (ex) {
 			ex.message.should.include('warn')
 		}
+	})
+
+	it('does not log anything before minThreshold laps', () => {
+		const stopWatch = new StopWatch({
+			logger: consoleStub,
+			watchMode: true,
+			minLaps: 10
+		})
+
+		for (let i = 1; i <= 10; i++) {
+			stopWatch.lap()
+		}
+
+		consoleStub.log.should.not.have.been.called
+		consoleStub.warn.should.not.have.been.called
+		consoleStub.error.should.not.have.been.called
+	})
+
+	const waitMs = ms => new Promise(resolve => setTimeout(resolve, ms))
+
+	describe('standard deviation', () => {
+		beforeEach(() => {
+			consoleStub.log.reset()
+			consoleStub.warn.reset()
+			consoleStub.error.reset()
+		})
+
+		const MS_ARRAY = [25,25,25,50,25,50,50,50,50,50]
+		const MEAN = 40
+		const STD_DEV = 12.247448713916
+
+		const awaitLaps = async msArray => {
+			const stopWatch = new StopWatch({
+				watchMode: true,
+				minLaps: msArray.length - 1 // only log for last value
+			})
+
+			for (const ms of msArray) {
+				await waitMs(ms)
+				stopWatch.lap('suf')
+			}
+		}
+
+		it('does not log if < 1 standard deviation away from mean time between laps', async () => {
+			await awaitLaps(MS_ARRAY)
+			consoleStub.log.should.not.have.been.called
+			consoleStub.warn.should.not.have.been.called
+			consoleStub.error.should.not.have.been.called
+		})
+		it('warns if between 1 and 2 standard deviations away from mean time between laps', async () => {
+			const warnArray = [...MS_ARRAY, MEAN + 1.5 * STD_DEV]
+			await awaitLaps(MS_ARRAY)
+			consoleStub.log.should.not.have.been.called
+			consoleStub.warn.should.have.been.called
+			consoleStub.error.should.not.have.been.called
+		})
+		it('errors if between >2 standard deviations away from mean time between laps', async () => {
+			const warnArray = [...MS_ARRAY, MEAN + 1.5 * STD_DEV]
+			await awaitLaps(MS_ARRAY)
+			consoleStub.log.should.not.have.been.called
+			consoleStub.warn.should.not.have.been.called
+			consoleStub.error.should.have.been.called
+		})
 	})
 }
